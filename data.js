2c013bdb0cb8565a3cb0a942522fd7738fbb685a
kaymeric <aymeric.kreuzer@ynov.com>
2023-05-06 18:58:53 +0200

    first commit

---
+++ data.js
@@ -0,0 +1,22 @@
+export const data = [
+    {
+        Title:"Projet1",
+        Desciption: "Lorem ipsum dolor sit amet consectetur adipisicing elitNihil expedita dignissimos doloremque laborum obcaecati sint voluptate architecto odio Sed quo perspiciatis unde alias magni sequi eius placeat deserunt Consequatur eius",
+        Image:"url",
+    },
+    {
+        Title:"Projet2",
+        Desciption: "Lorem ipsum dolor sit amet consectetur adipisicing elitNihil expedita dignissimos doloremque laborum obcaecati sint voluptate architecto odio Sed quo perspiciatis unde alias magni sequi eius placeat deserunt Consequatur eius",
+        Image:"url2",
+    },
+    {
+        Title:"Projet3",
+        Desciption: "Lorem ipsum dolor sit amet consectetur adipisicing elitNihil expedita dignissimos doloremque laborum obcaecati sint voluptate architecto odio Sed quo perspiciatis unde alias magni sequi eius placeat deserunt Consequatur eius",
+        Image:"url3",
+    },
+    {
+        Title:"Projet4",
+        Desciption: "Lorem ipsum dolor sit amet consectetur adipisicing elitNihil expedita dignissimos doloremque laborum obcaecati sint voluptate architecto odio Sed quo perspiciatis unde alias magni sequi eius placeat deserunt Consequatur eius",
+        Image:"url3",
+    },
+];
---
+++ menu.js
@@ -0,0 +1,78 @@
+import { data } from "./data.js";
+let div;
+let center = 0;
+let offset = 0;
+let direction = 0;
+
+document.querySelectorAll(".arrow").forEach(arrow => {
+    arrow.addEventListener("click", ()=>{
+        if (arrow.classList.contains("left")){
+            direction = -1;
+        } else {
+            direction = 1;
+        }
+        div =document.querySelector("#page");
+        offset = 0;
+        center -= direction;
+        debugCenter();
+        move();
+    })
+})
+
+function demoContent(){
+    for (let index = center-1; index <= center+1; index++) {
+        const element = data[getIndex(index)];
+        const parent = document.querySelector("#page");
+        const container = document.createElement("div");
+        const title = document.createElement("h3");
+        const desciption = document.createElement("div");
+        const img = document.createElement("img");
+        
+        title.innerText = element.Title;
+        desciption.innerText = element.Desciption;
+    
+        container.classList.add("demoContent");
+        container.appendChild(title);
+        container.appendChild(desciption);
+        container.appendChild(img);
+        parent.appendChild(container);
+    }
+}
+
+function move(){
+    if (Math.abs(offset)<34){
+        div.style.transform = `translate3d(${offset}%, 0, 0)`;
+        offset+=direction;
+        window.setTimeout(move,25);
+    } else {
+        deleteChild(div);
+        div.style.transform = `translate3d(0, 0, 0)`;
+        demoContent();
+        offset = 0;
+    }
+}
+
+function deleteChild(parent){
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function getIndex(index){
+    if(index<0){
+        return data.length+index
+    } else if (index>=data.length){
+        return index-data.length
+    }
+    return index
+}
+
+function debugCenter(){
+    if (center == data.length){ 
+        center = 0
+    } else if (center == -1){ 
+        center =  data.length-1
+    };
+}
+
+demoContent();
